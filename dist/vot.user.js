f8050aab0e0c9ca6de203d4ba9ac9666a0f8da0d
VitalyVl <78997248+vitalvlasyuk@users.noreply.github.com>
2023-11-08 02:49:27 +0200

    Update vot.user.js

--- dist/vot.user.js
+++ dist/vot.user.js
@@ -910,7 +910,7 @@ const sitesProxyTok = [
 /* harmony export */   iF: () => (/* binding */ workerHost)
 /* harmony export */ });
 // CONFIGURATION
-const workerHost = "api.browser.yandex.ru";
+const workerHost = "worker-misty-water-2866.capowe7591.workers.dev";
 const yandexHmacKey = "xtGCyGdTY2Jy6OMEKdTuXev3Twhkamgm";
 const yandexUserAgent =
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 YaBrowser/23.7.1.1140 Yowser/2.5 Safari/537.36";
